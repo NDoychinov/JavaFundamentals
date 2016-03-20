7de567a789bc838766f7fe083d3caa3b136df6b9
nikito873 <niki_genchev@abv.bg>
2016-03-20 18:57:29 +0200

    IntroJavaHomework

---
+++ JavaBasics/Intro-Java-Homework/src/AssignVariables.java
@@ -0,0 +1,14 @@
+
+public class AssignVariables {
+    public static void main(String[] args) {
+        byte byteNum = 127;
+        short shortNum = 32767;
+        int intNum = 2000000000;
+        long longNum = 919827112351L;
+        char someChar = 'c';
+        boolean trueOrFalse = false;
+        float floatNum = 0.5f;
+        double doubleNum = 0.1234567891011;
+        String someString = "Palo Alto, CA";
+    }
+}
---
+++ JavaBasics/Intro-Java-Homework/src/PrintCharacters.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Niki on 14.3.2016 г..
+ */
+public class PrintCharacters {
+    public static void main(String[] args) {
+        for (char chars = 'a'; chars <= 'z'; chars++) {
+            System.out.print(chars + " ");
+        }
+    }
+}
