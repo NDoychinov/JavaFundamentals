573740990b9530f72d4645f77b9ce4cc4eb606e1
nikito873 <niki_genchev@abv.bg>
2016-04-28 14:50:05 +0300

    Exam added

---
+++ Exam23April/Problem_1.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+/**
+ * Created by Niki on 23.4.2016 г..
+ */
+public class Problem_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.nextLine();
+        String patt = sc.nextLine();
+
+        while (true) {
+            boolean firstFound = false;
+            boolean lastFound = false;
+            String copyWord = word;
+            if (patt.length() > 0) {
+                for (int i = 0; i < copyWord.length() - patt.length() + 1; i++) {
+
+                    if (copyWord.substring(i, i + patt.length()).equals(patt)) {
+                        firstFound = true;
+                        copyWord = edit(copyWord, i, i + patt.length());
+                        break;
+                    }
+                }
+
+                for (int i = copyWord.length(); i > patt.length(); i--) {
+
+                    if (copyWord.substring(i - patt.length(), i).equals(patt)) {
+                        lastFound = true;
+                        copyWord = edit(copyWord, i - patt.length(), i);
+                        break;
+                    }
+                }
+            }
+
+            if (lastFound && firstFound) {
+                System.out.println("Shaked it.");
+                word = copyWord;
+                patt = edit(patt, patt.length() / 2, patt.length() / 2 + 1);
+
+            } else {
+                System.out.println("No shake.");
+                System.out.println(word);
+                break;
+            }
+        }
+    }
+
+    public static String edit(String str, int start, int end) {
+        String val1 = str.substring(0, start);
+        String val2 = str.substring(end, str.length());
+        String res = val1 + val2;
+        return res;
+    }
+}
---
+++ Exam23April/Problem_2.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+/**
+ * Created by Niki on 23.4.2016 г..
+ */
+public class Problem_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        sc.nextLine();
+
+        int[][] matrix = new int[row][col];
+
+        int count = 1;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = count;
+                count++;
+            }
+        }
+
+        String input = sc.nextLine();
+        while (!input.equals("Nuke it from orbit")) {
+            String[] data = input.split(" ");
+
+            int attRow = Integer.parseInt(data[0]);
+            int attCol = Integer.parseInt(data[1]);
+            int attRad = Integer.parseInt(data[2]);
+
+            if (isValid(matrix, attRow, attCol)) {
+                matrix[attRow][attCol] = 0;
+            }
+
+            for (int i = 1; i <= attRad; i++) {
+                if (isValid(matrix, attRow + i, attCol)) {
+                    matrix[attRow + i][attCol] = 0;
+                }
+                if (isValid(matrix, attRow - i, attCol)) {
+                    matrix[attRow - i][attCol] = 0;
+                }
+                if (isValid(matrix, attRow, attCol + i)) {
+                    matrix[attRow][attCol + i] = 0;
+                }
+                if (isValid(matrix, attRow, attCol - i)) {
+                    matrix[attRow][attCol - i] = 0;
+                }
+            }
+
+            int rowCount = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                int colCount = 0;
+                for (int j = 0; j < matrix[i].length; j++) {
+                    if (matrix[i][j] != 0) {
+                        int num = matrix[i][j];
+                        matrix[i][j] = 0;
+                        matrix[rowCount][colCount] = num;
+                        colCount++;
+                    }
+                }
+                if (colCount > 0) {
+                    rowCount++;
+                }
+            }
+
+            input = sc.nextLine();
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            boolean check = false;
+            String line = "";
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] != 0) {
+                    line += matrix[i][j] + " ";
+                    check = true;
+                }
+            }
+            if (check) {
+                System.out.println(line.trim());
+            }
+        }
+    }
+
+
+    public static boolean isValid(int[][] matrix, int row, int col) {
+        if ((matrix.length > row && row >= 0) && (matrix[row].length > col && col >= 0)) {
+            return true;
+        }
+        return false;
+    }
+}
